c74e7470498d4f0b161d4296b004e1715d7cba3c
tattedmich <michlinaresdominguez@gmail.com>
2023-04-10 10:13:52 +0200

    añadimos shopping bag para entrega 2

--- js/shopping.js
+++ js/shopping.js
@@ -3,7 +3,7 @@ class Shop {
         this.bag = shoppingBag
     }
     getTotalAmount(){
-        if (this.bag > 0) {
+        if (this.bag.length > 0) {
             return this.bag.reduce((acc, item) => acc + item.amount, 0)
         }
     }
